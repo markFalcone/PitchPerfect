fc7c7db4df42612663ac0ee8bbfd0e543767965e
markFalcone <falconemark16@yahoo.com>
2020-09-18 23:55:12 -0700

    Start Stop button
    Added a start stop button with changing instruction label. Embeded button and label into a stack view and pinned to center of the screen.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func recordAudio(_ sender: UIButton) {
+        print (sender.currentTitle)
+        if sender.currentTitle == "Start"{
+            sender.setTitle("Stop", for: .normal)
+            recordingLabel.text = "RECORDING"
+            print("in f")
+        } else {
+            sender.setTitle("Start", for: .normal)
+            recordingLabel.text = "Tap to Record"
+            print("in 2")
+        }
+        
+    }
+    
 
 }
 
